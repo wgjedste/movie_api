f6669536262046cdf791a16e9a0bdfdbe375a230
Will Gjedsted <wgjedste@gmail.com>
2021-04-23 12:39:50 -0700

    Disabled Origin

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-let allowedOrigins = ['http://localhost:8080', 'https://willsmovies.herokuapp.com'];
+// let allowedOrigins = ['http://localhost:8080', 'https://willsmovies.herokuapp.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 // mongoose.connect("mongodb://localhost:27017/test", {
