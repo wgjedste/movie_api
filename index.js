3aef8a2006515a9d3d876fc506cedcf8cd8d75b6
Will Gjedsted <wgjedste@gmail.com>
2021-05-01 13:12:29 -0700

    Added Authentication

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get("/", function (req, res) {
   res.send("Welcome to Flix Fix!");
 });
 
-app.get("/movies", function (req, res) {
+app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies);
